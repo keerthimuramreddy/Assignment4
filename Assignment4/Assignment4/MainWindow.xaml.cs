df37faef8a88c35adc8a3f963f96f4d670b5a5a4
kmuramreddy <kmuramreddy@US-GMD7XM3>
2024-02-14 18:26:52 +0530

    Add project files.

---
+++ Assignment4/Assignment4/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+using Xamarin.Forms.Xaml;
+
+namespace Assignment4
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainWindow : ContentPage
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            Label1.Text = "Font Size: " + Math.Truncate(e.NewValue);
+            Label2.FontSize = e.NewValue;
+
+        }
+    }
+}
